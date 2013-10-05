d4133d23ab2daf09ee4feba9c868f81e11af1ce3
Robert Marchman <robert.l.marchman@dartmouth.edu>
2013-10-05 14:34:26 -0400

    first template

--- signup/urls.py
+++ signup/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import patterns, url
 from signup import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<trip_id>\d+)/$', views.trip_detail, name='trip_detail'),
 )
--- signup/views.py
+++ signup/views.py
@@ -1,11 +1,36 @@
 
-
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.forms import ModelForm
+from django.views import generic
 
-from signup.models import Trip
+from signup.models import Trip, Trippee
 
 
-def index(request):
+def SignupForm(ModelForm):
+    """ Use to signup for a trip. """
+    class Meta:
+        model = Trippee
+        fields = ['name', 'email', 'dash', 'dietary_restrictions']
 
-    latest_trips = Trip.objects.order_by('-start_time')[:5]
-    return HttpResponse(' '.join([t.name for t in latest_trips]))
+
+class IndexView(generic.ListView):
+    """ Index of existing trips. """
+    
+    template_name = 'signup/index.html'
+    context_object_name = 'trip_list'
+    
+    def get_queryset(self):
+        return Trip.objects.all().order_by('-start_time')
+    
+    
+def trip_detail(request, trip_id):
+
+    trip = get_object_or_404(Trip, pk=trip_id)
+    return HttpResponse("name: %s" % trip.name)
+
+
+def signup(request, trip_id):
+
+    trip = get_object_or_404(Trip, pk=trip_id)
+    
